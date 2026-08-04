af34821cb894e4ab9440e4c644c75a52b9c4033b
Lourdes <trosch2003081@gmail.com>
2026-08-04 15:00:32 -0300

    agregando

---
+++ JS/asincrono.js
@@ -0,0 +1,30 @@
+console.log("inicio")
+setTimeout() => {
+    console.log("buscando alumno...")
+} , 3000}
+console.log("fin")
+
+function saludar () {
+    console.log("hola")
+}
+function ejecutar(funcion){
+    funcion();
+}
+function despedirse(){
+    console.log("hasta luego")
+}
+setTimeout(despedirse, 3000)
+
+setTimeout(() => {
+    console.log("buscando docentes...")
+} , 2000);
+
+setTimeout(() => {
+    console.log("")
+}
+
+console.log("abriendo SGA")
+setTimeout(() => {
+    console.log("alumnos cargados")
+}3000,)
+console.log("el ususario puede seguir navendo")
